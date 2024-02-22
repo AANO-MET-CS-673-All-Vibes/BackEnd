7e9ad8cb82fd35100ff0b6e6087cc072e5398bfa
jewelcodes <omarmelghoul01@gmail.com>
2024-02-22 10:49:19 -0500

    fetch username, email, and top songs

--- flaskr/login.py
+++ flaskr/login.py
@@ -1,12 +1,13 @@
 from flask import Flask, redirect, request, session
-import spotipy
+import spotipy, json
 from spotipy.oauth2 import SpotifyOAuth
 
 # Spotify API credentials
 SPOTIFY_CLIENT_ID = '710ea14ca1d949bda4e072ff4a4ebd21'
 SPOTIFY_CLIENT_SECRET = '0ff7573b7c0f4c7eaa987b983f81df84'
 SPOTIFY_REDIRECT_URI = 'http://127.0.0.1:5000/callback'  # TODO: Update with your redirect URI so that we can login via spotify as we discussed and not with google account and then connect the spotify account with it, Though i think we should keep the login via google and then connect the music apps account in the app itself(browser version) it is kind of a standard way if we are going to give support for spotify and apple music. See what you think NOUR
-SCOPE = 'user-library-read'  # TODO: For example scope i am asking to get the library of the user you can change it as you want NOUR.
+#SCOPE = 'user-library-read'  # TODO: For example scope i am asking to get the library of the user you can change it as you want NOUR.
+SCOPE = "user-top-read,user-read-email,user-read-private"     # i figured medium-term top songs are a better way to judge rather than liked songs that can accumulate songs from years ago you no longer like (?)
 
 def login():
     sp_oauth = SpotifyOAuth(client_id=SPOTIFY_CLIENT_ID, client_secret=SPOTIFY_CLIENT_SECRET, redirect_uri=SPOTIFY_REDIRECT_URI, scope=SCOPE)
@@ -25,7 +26,21 @@ def operations():
     token_info = session.get('token_info', None)
     if not token_info:
         return redirect('/login')
+
+    output = ""
     
     sp = spotipy.Spotify(auth=token_info['access_token'])
-    results = sp.current_user_saved_tracks()
-    return str(results)
+    profile = sp.me()
+    top_tracks = sp.current_user_top_tracks(limit=50, offset=0, time_range='medium_term')
+
+    output += "User name: " + profile["display_name"] + "<br>"
+    output += "Email: " + profile["email"] + "<br>"    # i think we can use this email to keep track of users instead of making them manually type it on a signup form
+
+    output += "Top 50 tracks from the past 6 months: <ol>"
+
+    for track in top_tracks["items"]:
+        output += "<li>" + track["name"] + " - " + track["artists"][0]["name"] + "</li>"
+
+    output += "</ol>"
+
+    return output
