6fbb0f950916c2a82c30d5ee9d35e79dde129578
jewelcodes <omarmelghoul01@gmail.com>
2024-02-25 16:49:49 -0500

    determine whether account already exists

--- flaskr/account.py
+++ flaskr/account.py
@@ -1,7 +1,19 @@
+import pymysql
 
 # exists(): this method determines if an account exists or not
 # Parameter: email - email associated with this account
 # Return: Boolean
 
 def exists(email):
-    return False    # placeholder
+    db = pymysql.connections.Connection(host="127.0.0.1", user="root", password="AllVibes01", database="allvibes")
+    cursor = db.cursor()
+    
+    count = cursor.execute("SELECT * FROM accounts WHERE email=\"" + email + "\"")
+
+    cursor.close()
+    db.close()
+
+    if count == 0:
+        return False
+    else:
+        return True
