eeee803e7db37057ef1dfa325bc6a411220c9800
jewelcodes <omarmelghoul01@gmail.com>
2024-03-16 15:27:58 -0400

    API: match recommendation system

--- flaskr/recs.py
+++ flaskr/recs.py
@@ -5,12 +5,52 @@ from flaskr import api, user, match
 
 db = pymysql.connections.Connection(host="127.0.0.1", user="root", password="AllVibes01", database="allvibes")
 
-# score(): returns a similarity score between two users' music taste
-# Parameters: rows from the "users" table
-# Returns: a number between 0 and 1
+# similarity(): returns a similarity score and top shared artists/tracks (up to 10) between two users
+# Parameters: user profile rows from the "users" table
+# Returns: a number between 0 and 1 and two arrays
 
-def score(user1, user2):
-    return 0.37     # test
+def similarity(user1, user2):
+    # count how many items are identical in each category
+    tracks1 = json.loads(user1[7])
+    artists1 = json.loads(user1[8])
+    tracks2 = json.loads(user2[7])
+    artists2 = json.loads(user2[8])
+
+    track_count = 0
+    artist_count = 0
+    track_weight = 0.5;
+    artist_weight = 0.5;
+
+    tracks = []
+    artists = []
+
+    for i in range(len(tracks1)):
+        for j in range(len(tracks2)):
+            if tracks1[i] == tracks2[j]:
+                if track_count < 10:
+                    tracks.append(tracks1[i])
+                track_count = track_count + 1
+
+    for i in range(len(artists1)):
+        for j in range(len(artists2)):
+            if artists1[i] == artists2[j]:
+                if artist_count < 10:
+                    artists.append(artists1[i])
+                artist_count = artist_count + 1
+
+    track_score = float(track_count) / float(len(tracks1))
+    artist_score = float(artist_count) / float(len(artists1))
+
+    if track_score > (1.2*artist_score):
+        track_weight = 0.75
+        artist_weight = 0.25
+    elif artist_score > (1.2*track_score):
+        track_weight = 0.25
+        artist_weight = 0.75
+
+    score = (track_weight*track_score)+(artist_weight*artist_score)
+
+    return score, artists, tracks
 
 # recs(): returns a list of recommendations to "swipe" on, each with a similarity score
 # This is essentially the core of the entire app
@@ -37,7 +77,10 @@ def recs():
         row = cursor.fetchone()
         if match.is_match(id, row[1]) == False:
             person["id"] = row[1]
-            person["score"] = score(user_profile, row)
+            score = similarity(user_profile, row)
+            person["score"] = score[0]
+            person["artists"] = score[1]
+            person["tracks"] = score[2]
 
         people.append(person)
 
