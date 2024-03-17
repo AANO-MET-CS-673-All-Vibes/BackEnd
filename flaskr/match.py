6be51f36a28a95bccceb9721920fa8ff60527817
jewelcodes <omarmelghoul01@gmail.com>
2024-03-16 21:00:08 -0400

    api: check if a given user is available to match

--- flaskr/match.py
+++ flaskr/match.py
@@ -11,7 +11,25 @@ db = pymysql.connections.Connection(host="127.0.0.1", user="root", password="All
 
 def is_match(id1, id2):
     cursor = db.cursor()
-    count = cursor.execute("SELECT * FROM matches WHERE (id1='" + id1 + "' and id2='" + id2 + "') OR (id1='" + id2 + "' and id2='" + id1 + "')")
+    count = cursor.execute("SELECT * FROM matches WHERE (matched=true AND (id1='" + id1 + "' and id2='" + id2 + "') OR (id1='" + id2 + "' and id2='" + id1 + "'))")
     cursor.close()
 
     return count != 0
+
+# is_available(): checks if an ID is available to be shown to another
+# Parameters: both INTERNAL IDs
+# Returns: Boolean
+
+def is_available(id1, id2):
+    cursor = db.cursor()
+
+    # IMPORTANT: in this query, we're swapping id1 and id2 for a reason, do NOT touch this
+    # the reasoning behind this is that we (id1) are the user looking for potential matches; so
+    # this only counts if a possible other user (id2) swapped right on us
+    # of course if the count returns zero then the user is automatically available to us
+    #
+    # tldr we're checking if ID2 is available to us, from the perspective of ID1
+    count = cursor.execute("SELECT * FROM matches WHERE (matched=false AND unmatched=false AND id1='" + id2 + "' and id2='" + id1 + "')")
+    cursor.close()
+
+    return count <= 1
