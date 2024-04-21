0f17901ef1d88d7ad0aa851679f0991e55bc346b
Adwait Kulkarni <adk1361@bu.edu>
2024-04-21 19:28:59 -0400

    update

--- database/key.sql
+++ database/key.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS key_table (
     id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL,
-    key VARCHAR(255) NOT NULL,
-    CONSTRAINT unique_email_key UNIQUE (email, key)
+    key_value VARCHAR(255) NOT NULL,
+    CONSTRAINT unique_email_key UNIQUE (email, key_value),
+    FOREIGN KEY (id) REFERENCES key_table (id)
 );
