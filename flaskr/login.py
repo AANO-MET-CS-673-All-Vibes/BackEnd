731caa20c9e6fb201ef23661cc9d331300c8ad56
jewelcodes <omarmelghoul01@gmail.com>
2024-04-29 16:08:15 -0400

    login: update redirects

--- flaskr/login.py
+++ flaskr/login.py
@@ -6,7 +6,7 @@ from flaskr import api, account
 # Spotify API credentials
 SPOTIFY_CLIENT_ID = '710ea14ca1d949bda4e072ff4a4ebd21'
 SPOTIFY_CLIENT_SECRET = '0ff7573b7c0f4c7eaa987b983f81df84'
-SPOTIFY_REDIRECT_URI = 'https://allvibes.jewelcodes.io/api/callback'  # TODO: Update with your redirect URI so that we can login via spotify as we discussed and not with google account and then connect the spotify account with it, Though i think we should keep the login via google and then connect the music apps account in the app itself(browser version) it is kind of a standard way if we are going to give support for spotify and apple music. See what you think NOUR
+SPOTIFY_REDIRECT_URI = 'https://allvibes.jewelcodes.io/callback'  # TODO: Update with your redirect URI so that we can login via spotify as we discussed and not with google account and then connect the spotify account with it, Though i think we should keep the login via google and then connect the music apps account in the app itself(browser version) it is kind of a standard way if we are going to give support for spotify and apple music. See what you think NOUR
 SPOTIFY_REDIRECT_WEB_URI = "https://allvibes.jewelcodes.io/callback"
 #SCOPE = 'user-library-read'  # TODO: For example scope i am asking to get the library of the user you can change it as you want NOUR.
 SCOPE = "user-top-read,user-read-email,user-read-private"     # i figured medium-term top songs are a better way to judge rather than liked songs that can accumulate songs from years ago you no longer like (?)
